e0e98b7294631050ce8a0197755c3d3f90627da6
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-08-21 16:04:10 +0530

    added  secondChanceItem API routes

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -7,6 +7,7 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 const app = express();
 app.use("*",cors());
@@ -27,7 +28,7 @@ app.use(express.json());
 //{{insert code here}}
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
