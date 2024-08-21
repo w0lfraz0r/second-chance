3ecfa635c9ca62c46e4dd97dd19b64374960f0cf
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-08-21 16:43:49 +0530

    added register user endpoint

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -9,6 +9,7 @@ const {loadData} = require("./util/import-mongo/index");
 
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 const searchRoutes = require('./routes/searchRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use("*",cors());
@@ -26,7 +27,7 @@ app.use(express.json());
 // Route files
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-//{{insert code here}}
+app.use('/api/auth', authRoutes);
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
